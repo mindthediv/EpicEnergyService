b235bc972ca240323c3cf717e35c5a7ea43572fe
Gabriele Pagliaricci <118124495+mindthediv@users.noreply.github.com>
2023-06-19 10:54:21 +0200

    spring initializr

---
+++ epic_energy_service/src/main/java/com/epic_energy/epic_energy_service/EpicEnergyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epic_energy.epic_energy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServiceApplication.class, args);
+	}
+
+}
---
+++ epic_energy_service/src/test/java/com/epic_energy/epic_energy_service/EpicEnergyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epic_energy.epic_energy_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
