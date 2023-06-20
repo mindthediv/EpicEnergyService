79710fdca43da9f4e4a5639b6d5a3e3396e01377
sabrizlt <sabrizaltini6@gmail.com>
2023-06-20 16:18:04 +0200

    ok

--- epic_energy_client/src/components/Home.jsx
+++ epic_energy_client/src/components/Home.jsx
@@ -5,8 +5,14 @@ import NavBar from "./NavBar";
 function BasicExample() {
   return (
     <>
-    <NavBar></NavBar>
-    
+      <NavBar></NavBar>
+
+      <div className="jumbotron">
+        <div className="mini-jumbotron mt-5">
+          <h2>ciao</h2>
+          <p>ciao</p>
+        </div>
+      </div>
     </>
   );
 }
