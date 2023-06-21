c28a1d2a96dabb7209e73c100bd6d639140383c0
VinceDV <122369775+VinceDV@users.noreply.github.com>
2023-06-21 16:42:28 +0200

    com

--- epic_energy_service/src/main/java/com/epic_energy/epic_energy_service/runner/csvRunner.java
+++ epic_energy_service/src/main/java/com/epic_energy/epic_energy_service/runner/csvRunner.java
@@ -1,22 +1,24 @@
-//  package com.epic_energy.epic_energy_service.runner;
-//  import java.io.BufferedReader;
-//  import java.io.FileReader;
-//  import java.util.ArrayList;
-//  import java.util.Arrays;
-//import java.util.List;
-//  import org.springframework.beans.factory.annotation.Autowired;
-//  import org.springframework.boot.ApplicationArguments;
-//  import org.springframework.boot.ApplicationRunner;
-//  import org.springframework.stereotype.Component;
-//  import com.epic_energy.epic_energy_service.models.Municipality;
-//  import com.epic_energy.epic_energy_service.models.Province;
-//  import com.epic_energy.epic_energy_service.repositories.MunicipalityRepository;
-//  import com.epic_energy.epic_energy_service.repositories.ProvinceRepository;
-//  import com.epic_energy.epic_energy_service.services.ProvinceService;
-//  import lombok.val;
-//  import lombok.extern.slf4j.Slf4j;
+// package com.epic_energy.epic_energy_service.runner;
+// import java.io.BufferedReader;
+// import java.io.FileReader;
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.ApplicationArguments;
+// import org.springframework.boot.ApplicationRunner;
+// import org.springframework.stereotype.Component;
+
+// import com.epic_energy.epic_energy_service.models.Municipality;
+// import com.epic_energy.epic_energy_service.models.Province;
+// import com.epic_energy.epic_energy_service.repositories.MunicipalityRepository;
+// import com.epic_energy.epic_energy_service.repositories.ProvinceRepository;
+// import com.epic_energy.epic_energy_service.services.ProvinceService;
+
+// import lombok.extern.slf4j.Slf4j;
 //  @Component
-//@Slf4j
+// @Slf4j
 //  public class csvRunner implements ApplicationRunner {
 //      @Autowired      ProvinceService provinceService;
 //      @Autowired
@@ -28,23 +30,23 @@
 //          List<List<String>> recordsProvince = new ArrayList<>();
 //  		     BufferedReader brProvince = new BufferedReader(
 //  		     new
-//  		     FileReader("C:\\Users\\Aless\\Desktop\\EpicEnergyService\\epic_energy_service\\src\\main\\resources\\province-italiane.csv"));
+//  		     FileReader("epic_energy_service\\\\src\\\\main\\\\resources\\\\province-italiane.csv"));
 //  		     String lineProvince;
 //  		     while ((lineProvince = brProvince.readLine()) != null) {
 // 		     String[] values = lineProvince.split(";");
-//		     recordsProvince.add(Arrays.asList(values));
-//		     Province p = new Province();
+// 		     recordsProvince.add(Arrays.asList(values));
+// 		     Province p = new Province();
 // 		     p.setSign(values[0]);
 //  		     p.setName(values[1]);
 // 		     p.setRegion(values[2]);
 //  		     System.out.println(p);
 //  		     provinceService.saveProvince(p);
-// 		     // log.info(values[0].toString() + " " + values[1].toString());
-// 		      records.forEach(el -> log.info(el.toString()));
-////}
+// 		     log.info(values[0].toString() + " " + values[1].toString());
+// 		    //records.forEach(el -> log.info(el.toString()));
+// }
 //          List<List<String>> records = new ArrayList<>();
 //          BufferedReader br = new BufferedReader(
-//                  new FileReader("C:\\Users\\Aless\\Desktop\\EpicEnergyService\\epic_energy_service\\src\\main\\resources\\comuni-italiani.csv"));
+//                  new FileReader("epic_energy_service\\\\src\\\\main\\\\resources\\\\comuni-italiani.csv"));
 //  			String line;
 //  			while ((line = br.readLine()) != null) {
 //  			    String[] values = line.split(";");
@@ -56,11 +58,11 @@
 // 		   // log.info(values[0].toString() + " " + values[1].toString() + " "
 //  		   //         + values[2].toString() + " ");		   
 // 		     }		     
-//      
+     
 //  			    throw new UnsupportedOperationException("Unimplemented method 'run'");
-//      
-//      
-//  
-//              
+     
+     
+ 
+             
 //              }
 //  }
